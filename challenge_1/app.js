77d7436681115dfa09e4f582b05b448e1ee1548b
Calvin <calvinminhla@gmail.com>
2021-01-25 21:27:30 -0800

    Added click event listener that alternates between 'X' and 'O'.

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,5 +1,27 @@
-/*
+
+console.log(`Connecting...
+You have connected app.js to index.html!`);
+
+let playerOne = true;
+
+const squares = document.querySelectorAll("span");
+
+squares.forEach(current => {
+  current.addEventListener('click', () => {
+    if (playerOne) {
+      current.innerHTML = 'X';
+      playerOne = false;
+    } else {
+      current.innerHTML = 'O';
+      playerOne = true;
+    }
+  })
+})
+
+
+/* Notes
+
+  * .querySelectorAll, .getElementsByClassName, and other methods return node list. They need to be iterarte via forEach() to access the elements
+  *
 
 */
-
-console.log('Hello');
