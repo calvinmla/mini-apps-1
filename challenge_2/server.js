99d75f9435620e09fd44a1dbb31550819491b345
Calvin <calvinminhla@gmail.com>
2021-01-29 16:23:42 -0800

    Set up server and index.html file

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+app.use(express.static('client'));
 
+app.post('/upload_json', (req, res) => {
+  console.log(req.body);
+  res.send(console.log('Received!'));
+  res.end();
+});
 
+app.listen(port, () => {
+  console.log(`Listing at http://localhost:${port}`)
+});
