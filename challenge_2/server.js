e619e923258fefd4ac9f73596ab5f10588145077
Calvin <calvinminhla@gmail.com>
2021-01-29 21:36:11 -0800

    Post request creates flattened CSV report

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 app.use(express.static('client'));
 
-app.post('/upload_json', (req, res) => {
-  console.log(req.body);
+app.use(bodyParser.urlencoded({extended: true }))
+app.use(bodyParser.json());
+
+app.post('/', (req, res) => {
+  let = csvReport = getColumnData(JSON.parse(req.body.data));
   res.send(console.log('Received!'));
   res.end();
 });
@@ -12,3 +16,21 @@ app.post('/upload_json', (req, res) => {
 app.listen(port, () => {
   console.log(`Listing at http://localhost:${port}`)
 });
+
+const getColumnData = (data) => {
+  let csvData = Object.keys(data);
+  csvData = csvData.slice(0, csvData.length - 1);
+  csvData = csvData.join(',') + '\n';
+
+  const getRowData = (object) => {
+    let rowData = Object.values(object);
+    rowData = rowData.slice(0, rowData.length - 1);
+    rowData = rowData.join(',') + '\n';
+    csvData = csvData + rowData;
+    for (let i = 0; i < object.children.length; i++) {
+      getRowData(object.children[i])
+    }
+  }
+  getRowData(data);
+  return csvData;
+}
