e5380b48c7a7f8ce95d7272913dad549caaf0a72
Calvin <calvinminhla@gmail.com>
2021-01-31 00:14:59 -0800

    Switched out express-fileupload with multer. Completed refactor to file picker.

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
+const multer = require('multer');
+const upload = multer({dest: 'uploads/'});
 const funcs = require('./client/app');
 const app = express();
 const port = 3000;
@@ -13,21 +15,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Need to look into why I don't need this
 // ---> app.use(bodyParser.json());
 
-// POST methods
-// app.post('/text_area_upload_json', (req, res) => {
-//   // Parse data and run function to convert data
-//   const csvReport = funcs.convertToCSV(JSON.parse(req.body.textAreaData));
-//   // Read html file and add converted JSON data
-//   fs.readFile('./client/index.html', 'utf8', (err, html) => {
-//     if (err) throw err;
-//     res.status(201).send(html + '</br>' + csvReport);
-//   })
-// });
 
-app.post('/file_picker_upload_json', (req, res) => {
-  const file = req.body;
-  console.log('file --->', file)
-  res.send(file);
+app.post('/upload_json', upload.single('jsonFile'), (req, res) => {
+  const filePath = req.file.path;
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) throw err;
+    const csvReport = funcs.convertToCSV(JSON.parse(data));
+    fs.readFile('./client/index.html', 'utf8', (err, data) => {
+      if (err) throw err;
+      res.status(201).send(data + '</br>' + csvReport);
+    })
+  })
+
 
   // // Uses node's File System to write to server
   // fs.writeFile('./xfile_picker.json', file, (err) => {
@@ -35,12 +34,6 @@ app.post('/file_picker_upload_json', (req, res) => {
   //   console.log('File has been saved.');
   // })
 
-  // fs.readFile('./file_picker.json', 'utf8', (err, data) => {
-  //   if (err) throw err;
-  //   console.log('data --->', data);
-  //   // const csvReport = funcs.convertToCSV(JSON.parse(data));
-  //   // console.log(csvReport)
-  // })
 
 });
 
@@ -59,3 +52,13 @@ app.listen(port, () => {
 //   res.status(200);
 // });
 
+// Text area POST method
+// app.post('/text_area_upload_json', (req, res) => {
+//   // Parse data and run function to convert data
+//   const csvReport = funcs.convertToCSV(JSON.parse(req.body.textAreaData));
+//   // Read html file and add converted JSON data
+//   fs.readFile('./client/index.html', 'utf8', (err, html) => {
+//     if (err) throw err;
+//     res.status(201).send(html + '</br>' + csvReport);
+//   })
+// });
