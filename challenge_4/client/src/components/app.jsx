2ef811b1e89688e21f9401da03e2afe5fad536cc
Calvin <calvinminhla@gmail.com>
2021-02-22 16:39:12 -0800

    Complete webpack set up along with connecting the client with the server

---
+++ challenge_4/client/src/components/app.jsx
@@ -0,0 +1,13 @@
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render () {
+    return (
+      <h1>Connect 4!</h1>
+    );
+  }
+}
+
+export default App;
